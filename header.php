929dad12bacd14eaf4076d0e30040276f9408d3c
katagiri <wazxcvbnm@icloud.com>
2020-12-02 11:05:35 +0900

    origin

--- header.php
+++ header.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
+
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/contact.css">
-        <link rel="stylesheet" href="css/phone.css">
-        <title>お問い合わせ</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="description" content="KEIBA naviでは、様々な競馬サイト（ホームページ）に関してナビゲーションさせていただきます。ご興味のあるサイトがございましたら、ぜひ各リンクをクリックしてみてください。">
+        <meta name="keywords" content="KEIBA navi,競馬,ナビゲーション">
+        <meta name="robots" content="noindex,nofollow">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <link rel="stylesheet" href="css/stylesheet-sp.css">
+        <link rel="stylesheet" href="css/flexslider.css" type="text/css">
+        <link rel="shortcut icon" type="image/x-icon" href="keibajou.ico">
+        <link rel="apple-touch-icon" href="images/home-icon.png">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="js/jquery.flexslider.js"></script>
+        <title>KEIBA navi</title>
     </head>
     <body>
         <div id="wrapper">
