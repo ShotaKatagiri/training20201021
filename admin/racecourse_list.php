dc73b42c018737a3f7140a13f60b5db4f1b63bb2
katagiri <wazxcvbnm@icloud.com>
2020-12-09 12:04:06 +0900

    task3sixth

--- admin/racecourse_list.php
+++ admin/racecourse_list.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
-
 if (empty($_SESSION['auth'])) {
     header('Location: login.php');
     exit;
 }
 
+require_once('Model.php');
 require_once('functions.php');
 
 ?>
-<?php require_once('header.php');?>
-<?php require_once('footer.php');?>
+<?php require_once('./header.php');?>
